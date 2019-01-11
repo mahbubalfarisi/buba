204d987bc9dd304b2efd721d20b30e8e21fda56e
Buba <mahbub.alfarisi@gmail.com>
2019-01-12 02:48:45 +0700

    simplify lampjs

--- index.php
+++ index.php
@@ -33,25 +33,7 @@
 			pointer-events: none;
 		}
 	</style>
-
-	<script type="text/javascript">
-		function toglamp() {
-		    var element = document.getElementById("_780833856");
-   			element.classList.toggle("fillamp");
-   			element.classList.toggle("fillampoff");
-
-   			var homeel = document.getElementById("homex");
-   			homeel.classList.toggle("dark");
-
-   			var homeel2 = document.getElementById("homex2");
-   			homeel2.classList.toggle("dark");
-   			homeel2.classList.toggle("clickof");
-
-   			var homeel2 = document.getElementById("notif");
-   			homeel2.classList.toggle("shownotiflamp");
-		};
-	</script>
-
+	<script src="./js/bubalamp.js"></script>
 	<head>
 		<title>Buba</title>
     </head>
